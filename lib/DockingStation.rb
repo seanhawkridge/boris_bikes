7f4be9451c70f173f6b554ab68c958ab46c01d8c
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-19 17:44:09 +0000

    challenge 12 done

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,17 +5,24 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-
+    @first_bike = true
   end
 
   def release_bike
+    if @first_bike
+      #bike = Bike.new
+      @first_bike = false
       Bike.new
+    elsif self.dock(bike) != nil
+      #bike = Bike.new
+      Bike.new
+    else
+        raise "No bike to release, use you eyes!"
+    end
   end
 
   def dock(bike)
     @bike = bike
   end
-
-
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,8 +8,9 @@ describe DockingStation do
      expect(dockingstation).to respond_to (:release_bike)
   end
 
-  it "initializes new bike when release_bike == true" do
-    expect((subject.release_bike).working?).to be_truthy
+  it "new bike.working? should return true" do
+    bike = subject.release_bike
+    expect(bike.working?).to be_truthy
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -25,6 +26,11 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  it "returns an error if the docking station does not have a bike docked" do
+    bike = subject.release_bike
+    expect {subject.release_bike}.to raise_error(RuntimeError)
   end
 
 end
