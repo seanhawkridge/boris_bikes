7a4d11010be313964617ca4194c419de5c06e3bd
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-19 14:12:46 +0000

    finished 10

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -5,26 +5,13 @@ describe DockingStation do
 
   subject(:dockingstation) {described_class.new}
 
-  # it "returns a new object when method .new called on it" do
-  #   expect(DockingStation.new).to eq true
-  # end
-
   it "returns release_bike == true when asked to release bike" do
      expect(dockingstation).to respond_to (:release_bike)
   end
 
   it "initializes new bike when release_bike == true" do
-    expect(dockingstation.release_bike).to be_instance_of Bike
+    expect((subject.release_bike).working?).to be_truthy
   end
 
-  it "returns release_bike == true when asked to release bike" do
-     expect(Bike).to respond_to (:working?)
-  end
-
-  # describe ".working?" do
-  #   it "returns true if bike is working" do
-  #     Bike.working?.should == true
-  #   end
-  # end
 
 end
